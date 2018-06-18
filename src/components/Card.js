da65fd2972bca1aa7fbb8547c54a5840faa27b93
Kenneth Brisco <kennethbrisco1@gmail.com>
2018-06-18 11:19:17 -0600

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//destructuring props
+const Card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}size=200x200`} alt="robot" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
